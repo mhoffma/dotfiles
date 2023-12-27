bc2abd5d8b964e3f99579dcb32dd2d19993fa4ce
marc <marc@penguin>
2023-12-27 06:31:51 -0500

    apt update

---
+++ apt.sh
@@ -0,0 +1,5 @@
+apt install htop zsh emacs tmux fzf
+apt install build-essential cmake autoconf autogen libtool shtool bear clangd
+apt install autoconf automake libtool texinfo build-essential xorg-dev libgtk2.0-dev libjpeg-dev libncurses5-dev libdbus-1-dev libgif-dev libtiff-dev libm17n-dev libpng-dev librsvg2-dev libotf-dev libgnutls28-dev libxml2-dev
+apt install x11-apps xterm
+apt install intel-basekit
